e0e352bbff53a88096e51c43c25479fb4b0546ee
endotakashi1992 <endotakashi1992@gmail.com>
2014-12-27 23:08:58 +0900

    install express

--- coffee/app.coffee
+++ coffee/app.coffee
@@ -1,1 +1,10 @@
-console.log "This is app.coffeeeeeeeeeeeeeeeeeeeeeeee"
+angular.module('myApp', ['ngRoute','ngResource'])
+.factory 'User', ($resource)->
+  return $resource '/api/users/:id'
+.controller 'tweets',($scope,User)->
+  console.log User
+  $scope.tweet = {}
+  $scope.tweets = []
+  $scope.tweet.text = "hai"
+  $scope.add = ->
+    $scope.tweets.push $scope.tweet
--- public/all.js
+++ public/all.js
@@ -1,4 +1,14 @@
 (function() {
-  console.log("This is app.coffeeeeeeeeeeeeeeeeeeeeeeee");
+  angular.module('myApp', ['ngRoute', 'ngResource']).factory('User', function($resource) {
+    return $resource('/api/users/:id');
+  }).controller('tweets', function($scope, User) {
+    console.log(User);
+    $scope.tweet = {};
+    $scope.tweets = [];
+    $scope.tweet.text = "hai";
+    return $scope.add = function() {
+      return $scope.tweets.push($scope.tweet);
+    };
+  });
 
 }).call(this);
